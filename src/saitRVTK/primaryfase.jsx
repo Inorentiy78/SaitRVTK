abb8381f9009c37e31bb9076dcfa387c8f320627
Inokentiy78 <148535452+Inorentiy78@users.noreply.github.com>
2023-12-22 18:45:23 +0500

    Update primaryfase.jsx

--- src/saitRVTK/primaryfase.jsx
+++ src/saitRVTK/primaryfase.jsx
@@ -1,15 +1,44 @@
-import React,  {useState} from "react";
+// PrimaryFase.jsx
 
-const PrimaryFase = () =>{
+import React from 'react';
+import './primary.css';
 
-    return (
+const PrimaryFase = () => {
+  return (
+    <div className="image-container">
+      <img className="imgs" src="https://avatars.mds.yandex.net/get-altay/4804646/2a00000179257afc634faffeca3001f9e233/XXXL" alt="" />
         <div>
-            <img src="/saitRVTK/imeg.img/priroda.jpg" alt="природа"></img>
-            <div>
-             <img src="obezyanka.jpg" alt="обезьянка"></img>
-            </div>
+          <p className='h1'>Преимущества:</p>
+            <p className='p1'><div>1. Наши учебные курсы состоят из широких спектров тем,</div>
+             <div>включая разработку программного обеспечения,</div> 
+             <div>тестирование и его поддержку.</div></p>
+                <img className='img1' src='https://topsov.com/wp-content/uploads/2023/02/scale_1200.jpg'></img>
+            <p className='p2'><div>2. Мы предоставляем возможность поступления по грантам, </div>
+            <div>содействуя студентам в достижении своих образовательных целей.</div></p>
+                <img className='img2' src='https://topsov.com/wp-content/uploads/2023/02/scale_1200.jpg'></img>
+            <p className='p3'><div>3. Наши учебные классы оборудованны современными компьютерами, </div>
+            <div>обеспечивающие практическое обучение на высоком уровне.</div></p>
+                <img className='img3' src='https://topsov.com/wp-content/uploads/2023/02/scale_1200.jpg'></img>
+            <p className='p4'><div>4. Преподавательский корпус состоит из опытных профессионалов в сфере IT, </div>
+            <div>готовые делиться своим опытом и знаниями.</div></p>
+                <img className='img4' src='https://topsov.com/wp-content/uploads/2023/02/scale_1200.jpg'></img>
+            <p className='p5'><div>5. Развитая студенческая общность и мероприятия,</div> 
+            <div>способствующие интеграции студентов и раскрытию их творческого потенциала.</div></p>
+                <img className='img5' src='https://topsov.com/wp-content/uploads/2023/02/scale_1200.jpg'></img>
+            
+             </div>
+      <div className="content-container">
+        <div className="text-overlay">
+          <i>Добро пожаловать в Республиканский Высший Технический Колледж!
+            Наше учебное заведение специализируется в области программного обеспечения, а также в других технических специальностях, предлагая высококачественные образовательные программы и возможность поступления по гранту.
+          </i>
         </div>
-    )
+        <p>Выберите возможность получить образование в области программного обеспечения и в других специальностях с помощью грантов, выбирая Республиканский Высший Технический Колледж для вашего будущего.</p>            Стройте будущее вместе с РВТК.
+            <p>Мы ждем вас!</p>
+      </div>
+    </div>
+  );
 }
 
 export default PrimaryFase;
+/*<img src= 'https://catherineasquithgallery.com/uploads/posts/2023-01/1674320390_catherineasquithgallery-com-p-serii-fon-pastelnii-foto-59.jpg'>*/ 
